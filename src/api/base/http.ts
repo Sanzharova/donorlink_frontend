7930c2a56302044e08fe466775e6e7cedf0c0242
Sanzharova <kimnamonb@gmail.com>
2025-12-15 00:45:49 +0600

    remake http.ts file to push user after 401 error

--- src/api/base/http.ts
+++ src/api/base/http.ts
@@ -1,32 +1,45 @@
-import axios from 'axios';
-import {toastError} from "@/utils/toast.ts";
+import axios from 'axios'
+import router from '@/router'
+import { toastError } from '@/utils/toast'
 
 const http = axios.create({
   baseURL: import.meta.env.VITE_API_URL || '',
   withCredentials: true,
-});
+})
 
 http.interceptors.request.use((config) => {
-  const token = localStorage.getItem('accessToken');
+  const token = localStorage.getItem('accessToken')
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`
   }
-  return config;
-});
+  return config
+})
 
 http.interceptors.response.use(
   (response) => response,
-  (error) => {
-    let msg = "Request error";
+  async (error) => {
+    const status = error.response?.status
 
-    if (error.response?.data?.message) msg = error.response.data.message;
-    else if (error.response?.data?.error) msg = error.response.data.error;
-    else if (error.message) msg = error.message;
+    if (status === 401) {
+      localStorage.removeItem('accessToken')
 
-    toastError(msg);
+      if (router.currentRoute.value.path !== '/login') {
+        await router.push('/login')
+      }
 
-    return Promise.reject(error);
+      return Promise.reject(error)
+    }
+
+    let msg = 'Request error'
+
+    if (error.response?.data?.message) msg = error.response.data.message
+    else if (error.response?.data?.error) msg = error.response.data.error
+    else if (error.message) msg = error.message
+
+    toastError(msg)
+
+    return Promise.reject(error)
   }
-);
+)
 
-export default http;
+export default http
